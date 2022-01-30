d84c762b9b8cab508088744f849fa3a1b58814de
DenisChertanov <dachertanov@edu.hse.ru>
2022-01-30 17:44:13 +0300

    Init commit for pal-recommendation-service

---
+++ pal-recommendation-service/src/main/java/dachertanov/pal/palrecommendationservice/PalRecomendationServiceApplication.java
@@ -0,0 +1,13 @@
+package dachertanov.pal.palrecommendationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalRecomendationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalRecomendationServiceApplication.class, args);
+    }
+
+}

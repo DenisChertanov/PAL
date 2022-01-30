a066615a73d17f6b73eb55ff8f3005453d8802cf
DenisChertanov <dachertanov@edu.hse.ru>
2022-01-30 17:38:07 +0300

    Initial commit with pal-backend-service

---
+++ pal-backend-service/src/main/java/dachertanov/pal/palbackendservice/PalBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package dachertanov.pal.palbackendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalBackendServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalBackendServiceApplication.class, args);
+    }
+
+}
---
+++ pal-backend-service/src/test/java/dachertanov/pal/palbackendservice/PalBackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dachertanov.pal.palbackendservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalBackendServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
